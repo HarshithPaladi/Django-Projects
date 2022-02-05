2c5c54a4ea60164f1ebc24a43605ef1122d1581e
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2022-02-05 12:50:55 +0530

    Random_Seq Features added

--- Random_Gen/urls.py
+++ Random_Gen/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.Random_gen, name='rand_init'),]
+urlpatterns = [path('', views.Random_gen, name='rand_init'),
+               path('csv/', views.Random_csv, name='rand_csv'),]
