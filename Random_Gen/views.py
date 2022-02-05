0ec9da0d63ebb82bf603600c820c83c79d1d9d6d
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2022-02-05 15:05:32 +0530

    test1

--- Random_Gen/views.py
+++ Random_Gen/views.py
@@ -4,6 +4,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 # Generate a sequence of random numbers with a given length n and maximum value m with no duplicates in the sequence and return the sequence as a csv file
+def logic(n,m):
+        # if n > m:
+        # return render(request, 'Random_Gen.html', {'error': 'The number of elements in the sequence should be less than the maximum value',"n":n,"m":m})
+        seq = []
+        while len(seq) < n - 1:
+            r = random.randint(-m, m - 1)  # Generate a random number between -m and m-1 so that the number is not in the sequence
+            if r not in seq:
+                seq.append(r)
+        seq.insert(random.randint(0, n - 1), m)  # Appending the maximum value to the sequence at a index randomly chosen
+                # random.shuffle(seq) # Shuffling the sequence # Deprecated due to better optimized solution found
+                # seq.sort()
+        lst = [str(i) for i in seq]
+        return lst
+
 def Random_gen(request):
     if request.method == "GET":
         return render(request, "rand_enter.html")
@@ -12,21 +26,7 @@ def Random_gen(request):
             n = int(request.POST["n"])
             m = int(request.POST["m"])
             if n > 0 and m > 0:
-                # if n > m:
-                #     return render(request, 'Random_Gen.html', {'error': 'The number of elements in the sequence should be less than the maximum value',"n":n,"m":m})
-                seq = []
-                while len(seq) < n - 1:
-                    r = random.randint(
-                        -m, m - 1
-                    )  # Generate a random number between -m and m-1 so that the number is not in the sequence
-                    if r not in seq:
-                        seq.append(r)
-                seq.insert(
-                    random.randint(0, n - 1), m
-                )  # Appending the maximum value to the sequence at a index randomly chosen
-                # random.shuffle(seq) # Shuffling the sequence # Deprecated due to better optimized solution found
-                # seq.sort()
-                lst = [str(i) for i in seq]
+                lst = logic(n,m)
                 res = render(request, "Random_Gen.html", {"lst": ",".join(lst)})
                 with open("Random_Gen/Random_Sequence.csv", "w") as f:
                     f.write(",".join(lst))
@@ -35,10 +35,9 @@ def Random_gen(request):
                 # return HttpResponse(','.join(lst))
                 return res
 
-                # return render(request, 'Random_Gen.html', {'seq': seq,"n":n,"m":m})
+            # return render(request, 'Random_Gen.html', {'seq': seq,"n":n,"m":m})
             else:
-                return render(
-                    request,
+                return render(request,
                     "Random_Gen.html",
                     {
                         "error": "The number of elements in the sequence should be greater than 0 and the maximum value should be greater than 0",
