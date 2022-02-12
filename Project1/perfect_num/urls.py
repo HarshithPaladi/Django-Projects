fddb4fec54d43e5624a9be4615f285f4f7078af4
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2022-02-12 15:31:53 +0530

    some changes

--- Project1/perfect_num/urls.py
+++ Project1/perfect_num/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', views.choose, name='choose'),
     path('divisors/', views.perfect_num, name='perfect_num'),
     path('num_stats/', views.num_stats, name='num_stats'),
+    path('amicable/', views.amicable, name='amicable_num'),
+    path('static_amicable/', views.static_amicable, name='static_amicable'),
 ]
--- Project1/perfect_num/views.py
+++ Project1/perfect_num/views.py
@@ -1,23 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-import math
 
+# def divisorGenerator(n):
+#     large_divisors = []
+#     for i in range(1, int(math.sqrt(n) + 1)):
+#         if n % i == 0:
+#             yield i
+#             if i*i != n:
+#                 large_divisors.append(n / i)
+#     for divisor in reversed(large_divisors):
+#         yield int(divisor)
 def divisorGenerator(n):
-    # lst = []
-    # i = 2
-    # while i <= math.sqrt(n):
-    #     if n%1 == 0:
-    #         if i == (n/i):
-    #             lst.append(i)
-    large_divisors = []
-    for i in range(1, int(math.sqrt(n) + 1)):
-        if n % i == 0:
-            yield i
-            if i*i != n:
-                large_divisors.append(n / i)
-    for divisor in reversed(large_divisors):
-        yield int(divisor)
+    Lst = [x for x in range(1, n) if n % x == 0]
+    return Lst
     
 def perfect_num(request):
     if request.method == "GET":
@@ -25,8 +22,8 @@ def perfect_num(request):
     try:
         if request.method == "POST":
             n = int(request.POST["n"])
-            lst = list(divisorGenerator(n))
-            lst.remove(n)
+            lst = divisorGenerator(n)
+            #lst.remove(n)
             return render(request, "perfect_num_out.html", {"lst": lst,"n":n})
         
     except Exception as f:
@@ -50,8 +47,8 @@ def num_stats(request):
             n1 = int(request.POST["n1"])
             n2 = int(request.POST["n2"])
             for i  in range(n1,n2+1):
-                lst = list(divisorGenerator(i))
-                lst.remove(i)
+                lst = divisorGenerator(i)
+                #lst.remove(i)
                 if sum(lst) == i:
                     perfect_num.append(i)
                 if sum(lst) < i:
@@ -67,3 +64,45 @@ def num_stats(request):
                 "error": f,
             },
         )
+def amicable(request):
+    if request.method == "GET":
+        return render(request, "num_stats_in.html")
+    try:
+        if request.method == "POST":
+            n1 = int(request.POST["n1"])
+            n2 = int(request.POST["n2"])
+            return HttpResponse("<h1>{}</h1>".format(check_amicable(n1,n2,True)))
+    except Exception as f:
+        return render(
+            request,
+            "num_stats_out.html",
+            {
+                "error": f,
+            },
+        )
+
+def check_amicable(n1,n2,x=False):
+    sum1 = sum(divisorGenerator(n1))
+    sum2 = sum(divisorGenerator(n2))
+    if x == True:
+        if sum1 == n2 and sum2 == n1:
+            return f"{n1} and {n2} are amicable"
+        else:
+            return f"{n1} and {n2} are not amicable"
+    else:
+        if sum1 == n2 and sum2 == n1:
+            return "Amicable"
+        else:
+            return "Not Amicable"
+
+def static_amicable(request):
+    if request.method == "GET":
+        dpairs = {}
+        pairs = [(150, 290), (220, 284), (1184, 1210), (1350, 1851)]
+        for x,y in pairs:
+            dpairs[(x,y)] = check_amicable(x,y)
+        return render(request, "static_amicable.html", {"pairs": dpairs.items()})
+            
+
+
+
